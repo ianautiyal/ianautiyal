7ad136a964902dd59ca659b7de18034381c150a1
Ajay Nautiyal <aju@nautiyal.dev>
2025-08-24 13:26:45 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Ajay Nautiyal - Developer | Debugger | Dad | Drifter | Dish Explorer</title>
+	<meta
+		name="description"
+		content="Ajay Nautiyal - Experienced Freelance Developer specializing in Python, TypeScript, PHP, and Dart. With over 7 years of expertise, I deliver high-quality solutions tailored to your needs."
+	/>
+</svelte:head>
+
+<div
+	class="relative -z-10 flex min-h-dvh flex-col items-center justify-center bg-[url(lib/assets/home-bg.jpeg)] bg-cover bg-center bg-no-repeat before:absolute before:inset-0 before:-z-10 before:bg-white before:opacity-70 before:dark:bg-gray-900"
+>
+	<h1 class="mb-2 font-serif text-8xl font-bold uppercase max-md:text-4xl">Ajay Nautiyal</h1>
+	<p class="tracking-widest max-md:text-sm">Developer | Debugger | Dad | Drifter | Dish Explorer</p>
+</div>
